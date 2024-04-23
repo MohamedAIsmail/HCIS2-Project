e3cbce613703e547219363ba6b4ac2d87996edea
MagdyNasr41 <magdynasr41@gmail.com>
2024-04-23 12:09:58 +0200

    healthcareProvider validaiton layer is done

--- services/server/EMR/routes/healthcareProviderRouter.js
+++ services/server/EMR/routes/healthcareProviderRouter.js
@@ -2,6 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const {
+    createHealthcareProviderValidator,
+    getHealthcareProviderValidator,
+    updateHealthcareProviderValidator,
+    deleteHealthcareProviderValidator
+} = require('../utils/validators/healthcareProviderValidator');
+
+const {
     createHealthcareProvider,
     getHealthcareProviders,
     getHealthcareProvider,
@@ -12,12 +19,12 @@ const {
 // Route to get all healthcare providers and create a new provider
 router.route("/")
     .get(getHealthcareProviders)
-    .post(createHealthcareProvider);
+    .post(createHealthcareProviderValidator, createHealthcareProvider);
 
 // Route to get, update, and delete a specific healthcare provider by ID
 router.route("/:id")
-    .get(getHealthcareProvider)
-    .put(updateHealthcareProvider)
-    .delete(deleteHealthcareProvider);
+    .get(getHealthcareProviderValidator, getHealthcareProvider)
+    .put(updateHealthcareProviderValidator, updateHealthcareProvider)
+    .delete(deleteHealthcareProviderValidator, deleteHealthcareProvider);
 
 module.exports = router;
---
+++ services/server/EMR/utils/validators/healthcareProviderValidator.js
@@ -0,0 +1,88 @@
+const { check, body } = require('express-validator');
+const slugify = require('slugify');
+
+const validatorMiddleware = require('../../middlewares/validatorMiddleware');
+const HealthcareProvider = require('../../models/healthcareProviderModel');
+
+exports.createHealthcareProviderValidator = [
+    check('username')
+        .notEmpty()
+        .withMessage('Username is required'),
+        
+    check('email')
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Invalid email address')
+        .custom(async (val) => {
+            return HealthcareProvider.findOne({ email: val }).then((found) => {
+                if (found) {
+                    return Promise.reject(new Error('Email is already in use'));
+                }
+            });
+        }),
+    
+    check('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+    
+    check('name')
+        .notEmpty()
+        .withMessage('Name is required'),
+    
+    check('specialization')
+        .notEmpty()
+        .withMessage('Specialization is required'),
+    
+    check('licenseNumber')
+        .notEmpty()
+        .withMessage('License number is required'),
+    
+    check('certifications')
+        .isArray({ min: 1 })
+        .withMessage('At least one certification is required'),
+
+    check('schedule')
+        .isObject()
+        .withMessage('Schedule must be an object'),
+
+    validatorMiddleware,
+];
+
+exports.updateHealthcareProviderValidator = [
+    check('id')
+        .isMongoId()
+        .withMessage('Invalid healthcare provider ID format'),
+
+    check('email')
+        .optional()
+        .isEmail()
+        .withMessage('Invalid email address')
+        .custom(async (val, { req }) => {
+            return HealthcareProvider.findOne({ email: val }).then((found) => {
+                if (found && found._id.toString() !== req.params.id) {
+                    return Promise.reject(new Error('Email is already in use'));
+                }
+            });
+        }),
+    
+    validatorMiddleware,
+];
+
+exports.getHealthcareProviderValidator = [
+    check('id')
+        .isMongoId()
+        .withMessage("Invalid member ID format"),
+
+    validatorMiddleware,
+];
+
+exports.deleteHealthcareProviderValidator = [
+    check('id')
+        .isMongoId()
+        .withMessage('Invalid Event ID Format'),
+
+    validatorMiddleware,
+];
