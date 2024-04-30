38cdf62c80ec5150db8b291f9b40f3200826a7d6
MohamedAIsmail <mohamedaismail214@gmail.com>
2024-04-30 18:24:38 +0300

    chore: router updates

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -2,9 +2,10 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/authentication/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Signup from "../pages/authentication/Signup";
-import Registerer from "../pages/Registerer/RegistererPortal";
+import Receptionist from "../pages/Receptionist/Receptionist";
 import AdminPage from "../pages/Admin/Admin";
 import Cookies from "js-cookie";
+import DoctorPage from "../pages/Doctor/Doctor";
 
 // Function to check if user is authenticated
 const isAuthenticated = () => {
@@ -29,11 +30,16 @@ const Router = () => {
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/registerpatient" element={<Registerer />} />
+            <Route path="/receptionist" element={<Receptionist />} />
             <Route
                 path="/admin-portal"
                 element={<ProtectedRoute element={<AdminPage />} />}
             />
+            <Route
+                path="/doctor-portal"
+                element={<ProtectedRoute element={<DoctorPage />} />}
+            />
+
             <Route path="/" element={<Login />} />
         </Routes>
     );
